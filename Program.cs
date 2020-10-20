1f1bd825a2a1017821856cec4903a838db85bae7
Lenodez <66466210+Lenodez@users.noreply.github.com>
2020-10-20 08:49:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppForTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
