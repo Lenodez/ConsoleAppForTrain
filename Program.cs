944db6d283be9d19c451dddc9f4988b486d44aee
Lenodez <66466210+Lenodez@users.noreply.github.com>
2020-10-20 13:29:54 +0300

    Создание массива произвольной длины

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleAppForTrain
 {
@@ -6,7 +7,19 @@ namespace ConsoleAppForTrain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] myArray; //создаем ссылку на массив
+            Console.Write("n= ");
+            int n = int.Parse(Console.ReadLine());
+            myArray = new int[n]; //выделяем память под массив требуемой длины
+            for (int i = 0; i < n; i++) //вводим элементы массива с клавиатуры
+            {
+                Console.Write("A[{0}]= ", i);
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            foreach (int elem in myArray) //выводим массив на экран
+            {
+                Console.Write("{0} ", elem);
+            }
         }
     }
 }
