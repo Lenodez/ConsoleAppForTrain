69cb4e81a6044e2e9671fa118cbd48c6e049a80f
Lenodez <66466210+Lenodez@users.noreply.github.com>
2020-10-20 13:39:32 +0300

    Создание простенькой функции

--- Program.cs
+++ Program.cs
@@ -1,25 +1,37 @@
 ﻿using System;
-using System.Linq;
 
 namespace ConsoleAppForTrain
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Print(int[] a)
         {
-            int[] myArray; //создаем ссылку на массив
-            Console.Write("n= ");
-            int n = int.Parse(Console.ReadLine());
-            myArray = new int[n]; //выделяем память под массив требуемой длины
-            for (int i = 0; i < n; i++) //вводим элементы массива с клавиатуры
-            {
-                Console.Write("A[{0}]= ", i);
-                myArray[i] = int.Parse(Console.ReadLine());
-            }
-            foreach (int elem in myArray) //выводим массив на экран
+            foreach (int elem in a)
             {
                 Console.Write("{0} ", elem);
             }
+            Console.WriteLine();
         }
+        //Заменяет отрицательные элементы массива а на нули. Размерность массива n.
+        static void Change(int[] a, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] < 0)
+                {
+                    a[i] = 0;
+                }
+            }
+        }
+        static void Main()
+        {
+            int[] myArray = { 0, -1, -2, 3, 4, 5, -6, -7, 8, -9 };
+            Console.Write("Исходный массив: ");
+            Print(myArray);
+            Change(myArray, 10);
+            Console.Write("Измененный массив: ");
+            Print(myArray);
+        }
+
     }
 }
