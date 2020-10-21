ad12667455db9ff1a8779d5d81d58dc53a8c4eb8
Lenodez <66466210+Lenodez@users.noreply.github.com>
2020-10-21 16:38:13 +0300

    начало разработки http клиента

--- Program.cs
+++ Program.cs
@@ -1,57 +1,32 @@
 ﻿using System;
+using System.Net;
 
 namespace ConsoleAppForTrain
 {
     class Program
     {
 
-        static void Print(int[,] a)
-        {
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    Console.Write("{0} ", a[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-        static void Input(out int[,] a)
-        {
-            Console.Write("n= ");
-            int n = int.Parse(Console.ReadLine());
-            Console.Write("m= ");
-            int m = int.Parse(Console.ReadLine());
-            a = new int[n, m];
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < a.GetLength(1); j++)
-                {
-                    Console.Write("a[{0},{1}]= ", i, j);
-                    a[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-        }
-        static void Change(int[,] a)
-        {
-            for (int i = 0; i < a.GetLength(0); i++)
-                for (int j = 0; j < a.GetLength(1); j++)
-                    if (a[i, j] % 2 == 0)
-                    {
-                        a[i, j] = 0;
-                    }
-        }
+
         static void Main()
         {
-            int[,] a;
-            Input(out a);
-            Console.WriteLine("Исходный массив:");
-            Print(a);
-            Change(a);
-            Console.WriteLine("Измененный массив:");
-            Print(a);
+            IPHostEntry host1 = Dns.GetHostEntry("www.microsoft.com");
+            Console.WriteLine(host1.HostName);
+            foreach (IPAddress ip in host1.AddressList)
+                Console.WriteLine(ip.ToString());
+
+            Console.WriteLine();
+
+            IPHostEntry host2 = Dns.GetHostEntry("google.com");
+            Console.WriteLine(host2.HostName);
+            foreach (IPAddress ip in host2.AddressList)
+                Console.WriteLine(ip.ToString());
+
+            IPHostEntry myPage = Dns.GetHostEntry("vk.com");
+            Console.WriteLine(myPage.HostName);
+            foreach (IPAddress ip in myPage.AddressList)
+                Console.WriteLine(ip.ToString());
         }
-        
+
 
 
 
